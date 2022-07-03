9c1bba7ee5afc1f0905ab274534206423cb7c7c1
HIEU LE DINH <HIEU.LD183530@sis.hust.edu.vn>
2022-07-03 17:07:34 +0700

    update model

--- model.py
+++ model.py
@@ -1,17 +1,36 @@
-import torch.nn as nn
 import torch
+from torchcrf import CRF
+import torch.nn as nn
+import pytorch_lightning as pl
 
+class LSTM_CRF(pl.LightningModule):
+    def __init__(self, in_dim, out_dim, 
+                num_labels: int,
+                hidden_size: int = 128,
+                dropout: float = 0.1,
+                learning_rate: float = 2e-5,
+                epsilon: float = 1e-8,
+                warmup_steps: int = 0,
+                weight_decay: float = 0.0,
+                train_batch_size: int = 32,
+                eval_batch_size: int = 32,
+                freeze_embed=False, 
+                num_val_dataloader: int = 1,
+                device=None
+    ):
 
-class LstmCRF(nn.Module):
-    def __init__(self, in_dim, out_dim, device=None):
         super().__init__()
         self.out_dim = out_dim
         self.device = device
+        self.num_labels = num_labels
+        self.num_val_dataloader = num_val_dataloader
 
         self.lstm0 = nn.LSTM(in_dim, hidden_size=32, num_layers=1, batch_first=True)
         self.lstm1 = nn.LSTM(input_size=32, hidden_size=64, num_layers=1, batch_first=True)
         self.lstm2 = nn.LSTM(input_size=64, hidden_size=128, num_layers=1, batch_first=True)
-        self.drop_out = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(p= dropout)
+
+        self.crf = CRF(self.num_labels, batch_first=self.hparams.batch_first)
 
         self.linear = nn.Sequential(nn.Linear(in_features=128, out_features=out_dim), nn.Tanh())
 
